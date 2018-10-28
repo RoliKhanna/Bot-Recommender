9c415240e22e48a501d77ffde91d91ff6afc1da5
Roli Khanna <roli.khanna1995@gmail.com>
2018-10-28 01:17:18 -0700

    (feat) Scrape logic writing and saving complete

--- data/scraper.py
+++ data/scraper.py
@@ -3,14 +3,16 @@ import json
 
 credentials = {}
 
-with open ("permission.json", "r") as file:
-    credentials = json.load(file)
+with open ("permission.json", "r") as readFile:
+    credentials = json.load(readFile)
 
 auth = tweepy.OAuthHandler(credentials["consumer_key"], credentials["consumer_secret"])
 auth.set_access_token(credentials["access_token"], credentials["access_token_secret"])
 
 api = tweepy.API(auth)
 
-public_tweets = api.home_timeline()
-for tweet in public_tweets:
-    print (tweet.text)
+tweets = api.user_timeline("year_progress")
+
+with open ("/Users/sandeepkhanna/Desktop/Fun/Bot_Recommender/data/raw/sampleTweets.json", "a") as writeFile:
+    for tweet in tweets:
+        writeFile.write('%s\n' %tweet)
